224588d569ec32d24c0ee2bf3a7da0c9516dd74d
pokus lang <pokuslang10@gmail.com>
2024-05-15 13:02:49 +0800

    site icon and title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Popsis') }}</title>
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/finalcroplogo1.png">
+    <title>Popsis Catering</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
